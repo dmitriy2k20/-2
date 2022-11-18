7b1b587217bad80ad44e658651386851def21e4f
dmitriy2k20 <118421220+dmitriy2k20@users.noreply.github.com>
2022-11-18 22:28:25 +0900

    Add files via upload

---
+++ practika/practika 18.11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace practika_18._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, sum;
+
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToInt32(Console.ReadLine());
+
+
+            sum = a + b;
+            Console.WriteLine("Ответ: " + sum);
+
+
+            float division;
+
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            division = a / b;
+            Console.WriteLine("Ответ: " +division);
+
+        }
+
+        private void Sum(float sum)
+        {
+            Console.WriteLine(+sum);
+        }
+        private void division(float sum)
+        {
+            Console.WriteLine(+sum);
+
+
+        }
+    }
+}
+
